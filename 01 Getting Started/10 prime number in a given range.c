abacef63b19a6ca3fc7ed5a0af3d6c24b424bbd3
Thanujasri M M <srithanuja164@gmail.com>
2024-08-24 15:01:40 +0530

    Create 10 prime number in a given range.c

---
+++ 01 Getting Started/10 prime number in a given range.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+
+void main() {
+    int min,max;
+    printf("enter value of min");
+    scanf("%d",&min);
+    printf("enter value of max");
+    scanf("%d",&max);
+ for(int i=min;i<=max;i++){
+     int count = 0; 
+       for(int j=1;j<=i;j++){
+          if(i%j==0){
+              count++;
+          }
+      }   
+        if(count==2){
+     printf("\n %d",i);
+    }
+  }  
+}
+O/P 
+    enter value of min3
+enter value of max15
+
+ 3
+ 5
+ 7
+ 11
+ 13
